10723bc7be1503a4fb07b7f24f468e66bf1aa5fb
Novath Thomas <57701433+pro-cms@users.noreply.github.com>
2022-03-02 02:47:57 +0300

    updated config file

--- config/zepsonsms.php
+++ config/zepsonsms.php
@@ -2,5 +2,9 @@
 
 
 return [
-    //
+
+        "from" => env('ZEPSON_SMS_FROM', ''),
+        "apiKey" => env('ZEPSON_SMS_API_KEY', ''),
+        "environment" => env('ZEPSON_SMS_ENVIRONMENT', 'sandbox'),
+
 ];

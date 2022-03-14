608999fcc943ecd93c8c5a2bc40aed9c600707f0
pro-cms <pro-cms@users.noreply.github.com>
2022-03-14 01:03:45 +0000

    Fix styling

--- src/ZepsonSmsChannel.php
+++ src/ZepsonSmsChannel.php
@@ -5,8 +5,9 @@ namespace NotificationChannels\ZepsonSms;
 use Exception;
 use Illuminate\Notifications\Notification;
 use NotificationChannels\ZepsonSms\Exceptions\CouldNotSendNotification;
-use ZepsonSms\SDK\ZepsonSms as ZepsonSmsSDK;
 use Str;
+use ZepsonSms\SDK\ZepsonSms as ZepsonSmsSDK;
+
 class ZepsonSmsChannel
 {
     /** @var ZepsonSmsSDK */
@@ -31,43 +32,40 @@ class ZepsonSmsChannel
         $phone_field = $message->getPhoneField();
 
         if (! $phoneNumber = $notifiable->routeNotificationFor('ZepsonSms')) {
-            $phoneNumber =  $this->formatContacts($notifiable->$phone_field, config('zepsonsms.country_code'));
+            $phoneNumber = $this->formatContacts($notifiable->$phone_field, config('zepsonsms.country_code'));
         }
 
         try {
-           dd( $this->ZepsonSms->sendSms([
+            dd($this->ZepsonSms->sendSms([
                 'recipient' => $phoneNumber,
                 'message' => $message->getContent(),
-                'sender_id' => $message->getSender()??config('zepsonsms.from'),
+                'sender_id' => $message->getSender() ?? config('zepsonsms.from'),
             ]));
         } catch (Exception $e) {
             throw CouldNotSendNotification::serviceRespondedWithAnError($e->getMessage());
         }
     }
 
-
-
-function formatContacts($contact, $countryCode){
-    $phoneNumbers = null;
+    public function formatContacts($contact, $countryCode)
+    {
+        $phoneNumbers = null;
 
         $totalDigits = Str::length($contact);
-        if($totalDigits==10){
+        if ($totalDigits == 10) {
             $phoneNumbers = $countryCode.Str::substr($contact, 1, 9);
         }
-        if($totalDigits==12){
+        if ($totalDigits == 12) {
             $phoneNumbers = $contact;
         }
-        if($totalDigits==13){
-            $phoneNumbers= $countryCode::substr($contact, 1, 13);
+        if ($totalDigits == 13) {
+            $phoneNumbers = $countryCode::substr($contact, 1, 13);
         }
-        if($totalDigits==14){
+        if ($totalDigits == 14) {
             $phoneNumbers = $countryCode.Str::substr($contact, 5, 14);
         }
 
 
-    // dd($phoneNumbers);
-    return $phoneNumbers;
-
-
-}
+        // dd($phoneNumbers);
+        return $phoneNumbers;
+    }
 }
